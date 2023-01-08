ee545f4c6b461b99dab9ba7b5ab030bc1a2a4cbf
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-01-08 18:45:28 +0530

    version 1.0

---
+++ 2index.py
@@ -0,0 +1,6 @@
+import random
+start_para=["hi,my name is","hi everyone this is","good morning everyone this is ","good evening everyone this is "]
+second_para=["shivam","chandan","aman","himanshu","anuj"]
+third_year=["and today","and here i am today to explain"," "]
+
+print(random.choice(start_para) +','+random.choice(second_para))
